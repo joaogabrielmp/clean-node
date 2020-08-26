941ad71c83d1417b340ae7080c1ea3016038031a
joaogabrielmp <joaogabrielma@gmail.com>
2020-08-26 16:06:55 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+
+import app from '@/main/config/app'
+
+describe('Body Parser Middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'User' })
+      .expect({ name: 'User' })
+  })
+})
