8f57b2e313f397b50b384d58496f0ed9539e5327
joaogabrielmp <joaogabrielma@gmail.com>
2020-11-20 15:23:35 -0300

    refactor: move Validation to protocols folder

--- src/main/factories/login/login-validation.spec.ts
+++ src/main/factories/login/login-validation.spec.ts
@@ -1,9 +1,8 @@
 import { makeLoginValidation } from './login-validation'
 import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
 import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { Validation } from '../../../presentation/helpers/validators/validation'
 import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
-import { EmailValidator } from '../../../presentation/protocols'
+import { EmailValidator, Validation } from '../../../presentation/protocols'
 
 jest.mock('../../../presentation/helpers/validators/validation-composite')
 
